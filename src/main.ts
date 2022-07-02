fec1f64281ec5d6cefc00f95757ecb049f65fa3e
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-07-02 19:06:53 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import './style.css'
+
+const state = {
+  emails: [
+    {
+      from: 'Nico',
+      header: "Link to today's video and slides is up!",
+      content:
+        'Link is up and you know where to find it! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
+      emailAddress: 'nico@email.com',
+      img: 'assets/nico.JPG',
+      read: false
+    },
+    {
+      from: 'Ed',
+      header:
+        "Congratulations! You have received a free beaver! Your name will now be displayed in the classroom's beaver list!",
+      content:
+        'Beaver beaver beaver beaver beaver beaver beaver beaver ! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
+      emailAddress: 'ed@email.com',
+      img: 'assets/ed.JPG',
+      read: false
+    },
+    {
+      from: 'Government',
+      header: 'Time to pay your tax!',
+      content:
+        'Pay us now! Pay us now! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci quo et assumenda voluptas blanditiis incidunt quia in, accusamus, qui voluptatem porro. Est reiciendis cum a architecto earum voluptatibus vel atque.',
+      emailAddress: 'government@email.com',
+      img: 'assets/gov.jpg',
+      read: false
+    }
+    // feel free to add more emails here
+  ]
+}
